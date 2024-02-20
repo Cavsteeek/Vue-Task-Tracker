8373d92e367a83c7c3536df5af7085c6a278235d
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-02-20 08:38:49 +0100

    added functionality to delete task and to confirm whether to delete task

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <Task :task="task" />
+        <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
     </div>
 </template>
 <script>
@@ -13,7 +13,8 @@ export default {
     },
     components:{
         Task
-    }
+    },
+    emits:['delete-task'],
 }
 </script>
 <style>
