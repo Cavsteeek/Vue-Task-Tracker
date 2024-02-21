ea2584900a86afdad5b0826e30069ebab151ee18
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-02-21 11:03:26 +0100

    feat: added toggle function to add Task button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,7 +11,7 @@ export default {
     },
     methods:{
        onClick(){
-        console.log(' ');
+        this.$emit('toggle-add-task')
        } 
     }
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button text="Add Task" color="green"/>
+        <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
         <!-- <Button text=" Update Task" color="blue"/>
         <Button text="Delete Task" color="red"/> -->
     </header>
@@ -9,11 +9,12 @@
 
 <script>
     import Button from './Button.vue'
-
+ 
     export default {
         name : 'Header',
         props: {
             title: String,
+            showAddTask: Boolean
         },
         // props: ['title'],
         components: {
