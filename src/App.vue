504564b8e6acccf74dc42acf2b2bd5b1cac61bc5
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-02-22 11:18:00 +0100

    feat: added footer component

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,10 @@
 
 <script>
 import Header from './components/Header.vue'
+
 import Tasks from './components/Tasks.vue'
 import AddTask from './components/AddTask.vue'
+
 
 export default {
   name: 'App',
